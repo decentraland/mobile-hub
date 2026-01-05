79a6a084801a9d8913d5bd2c0906c32f779dee67
gon boedo <gonzalo@decentraland.org>
2026-01-05 10:33:42 -0300

    fix basename path

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,20 @@ import { Navbar } from './components/Navbar'
 import { MapPage } from './pages/MapPage'
 import { config } from './config'
 
-// Base path injected by the server (see index.html)
-// When served from /hub/, the server rewrites __BASE_PATH__ to "/hub"
-declare global {
-  interface Window {
-    __BASE_PATH__?: string
-  }
-}
-
-const basePath = window.__BASE_PATH__ || ''
-
 const authConfig = {
   defaultChainId: Number(config.get('CHAIN_ID')) as ChainId,
   authUrl: config.get('AUTH_URL'),
-  basePath: basePath || '/mobile-curation'
+  basePath: '/mobile-hub'
 }
+const basename = /^decentraland.(zone|org|today)$/.test(window.location.host) ? '/mobile-hub' : '/'
 
 function App() {
   return (
-    <BrowserRouter basename={basePath}>
+    <BrowserRouter basename={basename}>
       <AuthProvider config={authConfig}>
         <Navbar />
         <Routes>
-          <Route path="/map" element={<MapPage />} />
+          <Route path="/" element={<MapPage />} />
           <Route path="*" element={<Navigate to="/map" replace />} />
         </Routes>
       </AuthProvider>
