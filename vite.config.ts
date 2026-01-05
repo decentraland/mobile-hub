a8d79c790a50d37ffc1ff3f656115fd722dacb73
gon boedo <gonzalo@decentraland.org>
2026-01-05 10:18:21 -0300

    fix: build command:

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime']
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+export default defineConfig(({ command, mode }) => {
+  const envVariables = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': {
+        VITE_REACT_APP_DCL_DEFAULT_ENV: envVariables.VITE_REACT_APP_DCL_DEFAULT_ENV,
+      }
+    },
+    ...(command === 'build' ? { base: envVariables.VITE_BASE_URL } : undefined),
   }
 })
